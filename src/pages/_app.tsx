71eb16c3923bff991d54f3fb92b034e5c3ec624f
Marc Doerflinger <marc@doerf.li>
2023-01-21 14:16:52 +0000

    add mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Container, CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Harmonograph</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CssBaseline enableColorScheme />
+      <Container maxWidth="lg" sx={{ p: 1 }}>
+        <Component {...pageProps} />
+      </Container>
+    </React.Fragment>
+  );
 }
