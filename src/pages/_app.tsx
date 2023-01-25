3b55d8f31b9a86a3dab637c17aab56412ad6a918
Marc Doerflinger <marc@doerf.li>
2023-01-25 09:29:04 +0000

    Add github link (closes #16)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ import Head from 'next/head';
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import dynamic from 'next/dynamic'
-import { Grid } from '@mui/material'
+import { Button, Grid } from '@mui/material'
 import Config from '@/components/config'
-import { useSelector } from 'react-redux'
-import { RootState } from '@/redux/store'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Harmonograph = dynamic(() => import('../components/harmonograph').then((mod) => mod.default), {
   ssr: false,
@@ -23,6 +23,12 @@ export default function Home() {
         </Grid>
         <Grid item xs={9}>
           <Harmonograph />
+        </Grid>
+        <Grid item xs={12} sx={{ p: 1 }}>
+          <Button href='https://github.com/doerfli/harmonograph' target="_blank">
+            <FontAwesomeIcon icon={faGithub} className="fa cursor-pointer" />
+            Github Repository
+          </Button>
         </Grid>
       </Grid>
     </>
