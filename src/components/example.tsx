e1ff3caf385514d96d9bcf4a940da05148bc66b0
Marc Doerflinger <marc@doerf.li>
2023-01-31 22:04:57 +0000

    don't use absolute url for examples (#8)

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,14 +1,15 @@
 import { Card, CardActionArea, CardContent, CardMedia, Tooltip, Typography } from "@mui/material";
 
 export default function Example(props: any) {
-    const { name, file, url } = props;
+    const { name, file, slug } = props;
     
     const filename = "examples/" + file;
+    const path = "/c/" + slug;
 
     return (    
         <Tooltip title="Click to Open" enterDelay={1000} enterNextDelay={1000} >
             <Card>
-                <CardActionArea href={url}>
+                <CardActionArea href={path}>
                     <CardMedia
                         component="img"
                         height="200"
--- src/pages/examples.tsx
+++ src/pages/examples.tsx
@@ -11,72 +11,72 @@ export default function Examples() {
         {
             name: "Example 1",
             file: "01.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSw5NiwwOzYsMjIsMC41fDAuMDEsMyw2MCw="
+            slug: "djEkMSw5NiwwOzYsMjIsMC41fDAuMDEsMyw2MCw="
         },
         {
             name: "Example 2",
             file: "02.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSw5NiwwOzIsMzQsMS4yNXwwLjAxLDMsNjAs"
+            slug: "djEkMSw5NiwwOzIsMzQsMS4yNXwwLjAxLDMsNjAs"
         },
         {
             name: "Example 3",
             file: "03.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSw5NiwwOzQsMzQsMXwwLjAxLDMsNjAs"
+            slug: "djEkMSw5NiwwOzQsMzQsMXwwLjAxLDMsNjAs"
         },
         {
             name: "Example 4",
             file: "04.png",
-            url: "https://harmonograph.bytes.li/c/djEkMiw5NiwwOzYsMzQsMXwwLjAxLDMsNjAs"
+            slug: "djEkMiw5NiwwOzYsMzQsMXwwLjAxLDMsNjAs"
         },
         {
             name: "Example 5",
             file: "05.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSw1MCwwOzEsNTAsMC41fDIsNDksMDsyLDYxLDAuNXwwLjAxLDMsNjAs"
+            slug: "djEkMSw1MCwwOzEsNTAsMC41fDIsNDksMDsyLDYxLDAuNXwwLjAxLDMsNjAs"
         },
         {
             name: "Example 6",
             file: "06.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSwxMDAsMDsxLDEwMCwwLjV8OCwyMCwwOzgsMjAsMC41fDAuMDEsMyw2MCw="
+            slug: "djEkMSwxMDAsMDsxLDEwMCwwLjV8OCwyMCwwOzgsMjAsMC41fDAuMDEsMyw2MCw="
         },
         {
             name: "Example 7",
             file: "07.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSwxMDAsMDsxLDEwMCwwLjV8NSwyMCwwOzMsNzUsMC41fDAuMDEsMyw2MCw="
+            slug: "djEkMSwxMDAsMDsxLDEwMCwwLjV8NSwyMCwwOzMsNzUsMC41fDAuMDEsMyw2MCw="
         },
         {
             name: "Example 8",
             file: "08.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSwxMDAsMDsxLDEwMCwwLjV8NywyMCwwOzIsODQsMC41fDAuMDEsMyw2MCw="
+            slug: "djEkMSwxMDAsMDsxLDEwMCwwLjV8NywyMCwwOzIsODQsMC41fDAuMDEsMyw2MCw="
         },
         {
             name: "Example 9",
             file: "09.png",
-            url: "https://harmonograph.bytes.li/c/djEkMiwxMDAsMDsyLDYzLDAuNXwzLDc5LDA7Niw0MywwLjV8MC4wMSwzLDYwLA=="
+            slug: "djEkMiwxMDAsMDsyLDYzLDAuNXwzLDc5LDA7Niw0MywwLjV8MC4wMSwzLDYwLA=="
         },
         {
             name: "Example 10",
             file: "10.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSw1MCwwOzEsNTAsMC41fDMsNTAsMDszLDUwLDAuNXw0LDUwLDA7NCw1MCwwLjV8Niw1MCwwOzYsNTAsMC41fDAuMDEsMyw2MCw="
+            slug: "djEkMSw1MCwwOzEsNTAsMC41fDMsNTAsMDszLDUwLDAuNXw0LDUwLDA7NCw1MCwwLjV8Niw1MCwwOzYsNTAsMC41fDAuMDEsMyw2MCw="
         },
         {
             name: "Example 11",
             file: "11.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSw1MCwwOzEsNTAsMC41fDMsNTAsMDszLDUwLDAuNXw2LDMxLDA7Niw1MCwwLjV8Myw1MCwwOzcsNTAsMC4yNXwwLjAxLDMsNjAs"
+            slug: "djEkMSw1MCwwOzEsNTAsMC41fDMsNTAsMDszLDUwLDAuNXw2LDMxLDA7Niw1MCwwLjV8Myw1MCwwOzcsNTAsMC4yNXwwLjAxLDMsNjAs"
         },
         {
             name: "Example 12",
             file: "12.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSw1MCwwOzEsNTAsMC41fDMsNTAsMDsyLDUwLDAuNXw2LDMxLDA7Miw1MCwwLjV8Nyw1MCwwOzIsNTAsMC4yNXwwLjAxLDMsNjAs"
+            slug: "djEkMSw1MCwwOzEsNTAsMC41fDMsNTAsMDsyLDUwLDAuNXw2LDMxLDA7Miw1MCwwLjV8Nyw1MCwwOzIsNTAsMC4yNXwwLjAxLDMsNjAs"
         },
         {
             name: "Example 13",
             file: "13.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSwxMDAsMDsxLDUwLDAuNXwxLDEwMCwwOzEsMzMsMC41fDEsNjYsMDs1LDQ2LDAuNXwwLjAxLDMsNjAs"
+            slug: "djEkMSwxMDAsMDsxLDUwLDAuNXwxLDEwMCwwOzEsMzMsMC41fDEsNjYsMDs1LDQ2LDAuNXwwLjAxLDMsNjAs"
         },
         {
             name: "Example 14",
             file: "14.png",
-            url: "https://harmonograph.bytes.li/c/djEkMSwxMDAsMDsxLDUwLDAuNXwxLDEwMCwwOzEsMzMsMC41fDMsNjYsMDs2LDQ2LDEuNzV8MC4wMSwzLDYwLA=="
+            slug: "djEkMSwxMDAsMDsxLDUwLDAuNXwxLDEwMCwwOzEsMzMsMC41fDMsNjYsMDs2LDQ2LDEuNzV8MC4wMSwzLDYwLA=="
         },
     ];
 
@@ -84,7 +84,7 @@ export default function Examples() {
         <Grid container spacing={4}>
             {[examples.map((e, i) =>
                 <Grid item key={i} xs={12} md={4} sx={{ p: 1 }} > 
-                    <Example name={e.name} file={e.file} url={e.url} />
+                    <Example name={e.name} file={e.file} slug={e.slug} />
                 </Grid>
             )]}
         </Grid>
